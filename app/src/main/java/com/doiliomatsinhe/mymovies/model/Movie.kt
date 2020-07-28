440f084796ffaf04057b1ec689453b69bf27dc61
doilio <doiliomatsinhe@hotmail.com>
2020-07-28 21:07:42 +0200

    Remove unused code
    Signed-off-by: doilio <doiliomatsinhe@hotmail.com>

--- app/src/main/java/com/doiliomatsinhe/mymovies/model/Movie.kt
+++ app/src/main/java/com/doiliomatsinhe/mymovies/model/Movie.kt
@@ -1,8 +1,5 @@
 package com.doiliomatsinhe.mymovies.model
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.map
-import com.doiliomatsinhe.mymovies.data.db.DatabaseMovie
 import com.google.gson.annotations.SerializedName
 
 data class Movie(
@@ -24,47 +21,4 @@ data class Movie(
 ) {
     val fullPosterPath: String
         get() = "http://image.tmdb.org/t/p/w342$poster_path"
-}
-
-fun List<DatabaseMovie>.asDomainModel(): List<Movie> {
-
-    return map {
-        Movie(
-            popularity = it.popularity,
-            vote_count = it.vote_count,
-            video = it.video,
-            poster_path = it.poster_path,
-            id = it.id,
-            adult = it.adult,
-            backdrop_path = it.backdrop_path,
-            original_language = it.original_language,
-            original_title = it.original_title,
-            genre_ids = it.genre_ids,
-            title = it.title,
-            vote_average = it.vote_average,
-            overview = it.overview,
-            release_date = it.release_date
-        )
-    }
-}
-
-fun LiveData<DatabaseMovie>.asDomainModel(): LiveData<Movie> {
-    return map {
-        Movie(
-            popularity = it.popularity,
-            vote_count = it.vote_count,
-            video = it.video,
-            poster_path = it.poster_path,
-            id = it.id,
-            adult = it.adult,
-            backdrop_path = it.backdrop_path,
-            original_language = it.original_language,
-            original_title = it.original_title,
-            genre_ids = it.genre_ids,
-            title = it.title,
-            vote_average = it.vote_average,
-            overview = it.overview,
-            release_date = it.release_date
-        )
-    }
 }
