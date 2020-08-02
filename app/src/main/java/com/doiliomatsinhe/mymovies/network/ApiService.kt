6e7ab7f3942dcefd2892a5ea717c461226edb92c
doilio <doiliomatsinhe@hotmail.com>
2020-08-02 14:56:40 +0200

    Add Api Queries for TvSeries
    Signed-off-by: doilio <doiliomatsinhe@hotmail.com>

--- app/src/main/java/com/doiliomatsinhe/mymovies/network/ApiService.kt
+++ app/src/main/java/com/doiliomatsinhe/mymovies/network/ApiService.kt
@@ -46,5 +46,27 @@ interface ApiService {
         @Query(PAGE) page: Int
     ): NetworkTvSeries
 
+    @GET("genre/tv/list")
+    suspend fun getTvGenres(
+        @Query(API_KEY) apiKey: String
+    ): NetworkTvGenres
+
+    @GET("tv/{tv_id}/reviews")
+    suspend fun getTvReview(
+        @Path(TV_ID) tvId: Int,
+        @Query(API_KEY) apiKey: String
+    ): NetworkTvReview
+
+    @GET("tv/{tv_id}/credits")
+    suspend fun getTvCredits(
+        @Path(TV_ID) tvId: Int,
+        @Query(API_KEY) apiKey: String
+    ): NetworkTvCredit
+
+    @GET("tv/{tv_id}/videos")
+    suspend fun getTvTrailers(
+        @Path(TV_ID) tvId: Int,
+        @Query(API_KEY) apiKey: String
+    ): NetworkTvTrailer
 }
 
